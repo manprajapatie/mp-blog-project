be5364d8ca36b96698fc56f1200c72aa10fbae51
manprajapatie <manprajapatie@gmail.com>
2025-06-06 01:50:42 +0530

    structure Header

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <header className="bg-green-500 text-white p-4 shadow-md">
+        <h1 className="text-2xl font-bold">Recipe Blog</h1>
+      </header>
+    </>
+  )
+}
+
+export default Header
