1dbb68394b50b3e4de04b17944634acc1142c253
manprajapatie <manprajapatie@gmail.com>
2025-06-09 21:47:41 +0530

    remove data from Home

--- frontend/src/features/recipes/pages/Home.jsx
+++ frontend/src/features/recipes/pages/Home.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 
 const Home = () => {
+
+  
+
   return (
-    <div>
-      
-    </div>
+    <>
+      {/* <div className='min-h-screen bg-gray-100 py-10 px-4'>
+        <h1 className='text-5xl font-bold mb-8 text-center'>Recipe Blog</h1>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
+          {recipes.map((recipe) => (
+            <div
+              key={recipe.id}
+              className="bg-white rounded-2xl shadow-md hover:shadow-xl transition duration-300"
+            >
+              <img
+                src={recipe.image}
+                alt={recipe.title}
+                className="rounded-t-2xl w-full h-48 object-cover"
+              />
+              <div className="p-4">
+                <h2 className="text-xl font-semibold mb-2">{recipe.title}</h2>
+                <div className="flex flex-wrap gap-2">
+                  {recipe.tags.map((tag, index) => (
+                    <span
+                      key={index}
+                      className="bg-green-100 text-green-700 text-xs font-medium px-2 py-1 rounded-full"
+                    >
+                      {tag}
+                    </span>
+                  ))}
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div> */}
+    </>
   )
 }
 
