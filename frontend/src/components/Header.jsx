32cd53775a0dca0b8c86ed208e032f4ccab2f5a6
manprajapatie <manprajapatie@gmail.com>
2025-06-28 00:55:15 +0530

    add nav in header (home fav)

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
     <>
-      <header className="bg-green-500 text-white p-4 shadow-md">
+      <header className="bg-green-500 text-white p-4 shadow-md flex place-content-between">
         <h1 className="text-2xl font-bold">Recipe Blog</h1>
+        <nav className='flex space-x-12 px-50 text-2xl'>
+       <Link to="/" className='hover:underline'>Home</Link>
+       <Link to="/Favorites" className='hover:underline'>Favorite</Link>
+        </nav>
       </header>
     </>
   )
