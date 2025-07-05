fb5173545795322d27c05f1527fbecfc4fdac9de
manprajapatie <manprajapatie@gmail.com>
2025-07-05 22:58:43 +0530

    create reducer for login logout

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,6 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-// const initialState = {
-//     user: ""   ,
-//     isAuthenticated: 
-// }
+const initialState = {
+    user: null,
+    isAuthenticated: null,
+    error: null,
+};
+
+const authSlice = createSlice({
+    name: auth,
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.user = action.payload;
+            state.isAuthenticated = true;
+            state.error = null;
+        },
+        logout: (state) => {
+            state.user = null;
+            state.isAuthenticated = false;
+            state.error = null;
+        },
+        setError: (state, action) =>{
+            state.error = action.payload
+        }
+    }
+})
+
+export const {login, logout, setError} = authSlice.reducer
+export default authSlice.reducer;
