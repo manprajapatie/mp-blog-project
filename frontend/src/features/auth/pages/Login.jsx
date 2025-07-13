80657bbd0ed8a6b0572d6cf74ca262005679f9ad
manprajapatie <manprajapatie@gmail.com>
2025-07-14 00:36:55 +0530

    create Login page

---
+++ frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from "react-redux"
+import { login, setError } from '../authSlice'
+import { useNavigate } from "react-router-dom"
+
+const Login = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default Login
