4ce5ee46cade1902eb80790c49e7bb2b6118c8ac
manprajapatie <manprajapatie@gmail.com>
2025-07-19 22:31:13 +0530

    save user login info in localstorage

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,24 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const userFromStorage = JSON.parse(localStorage.getItem("user"))
+
 const initialState = {
-    user: null,
-    isAuthenticated: null,
+    user: userFromStorage || null,
+    isAuthenticated: !!userFromStorage,
+    //!! it is use to clean code, it give us true or false(if null it automaticly convert it into false)
     error: null,
 };
 
 const authSlice = createSlice({
-    name: auth,
+    name: "auth",
     initialState,
     reducers: {
         login: (state, action) => {
             state.user = action.payload;
             state.isAuthenticated = true;
             state.error = null;
+            localStorage.setItem("user", JSON.stringify(action.payload))
+            //localStorage Take data in String only
         },
         logout: (state) => {
             state.user = null;
             state.isAuthenticated = false;
             state.error = null;
+            localStorage.removeItem("user") //remove user when logout
         },
         setError: (state, action) =>{
             state.error = action.payload
@@ -26,5 +32,5 @@ const authSlice = createSlice({
     }
 })
 
-export const {login, logout, setError} = authSlice.reducer
+export const {login, logout, setError} = authSlice.actions
 export default authSlice.reducer;
