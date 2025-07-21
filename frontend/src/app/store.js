d8b7a3e7115683f208b2c4d5bdba22311d934f82
manprajapatie <manprajapatie@gmail.com>
2025-07-21 23:28:06 +0530

    add auth store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,11 @@
 import {configureStore} from '@reduxjs/toolkit'
 import recipeReducer from '../features/recipes/recipeSlice';
+import authReducer from '../features/auth/authSlice';
 
 export const store = configureStore({
     reducer: {
         recipes: recipeReducer,
+        auth: authReducer, //we can name whatever we want, just we have to import thinks with same name
     },
 });
 
