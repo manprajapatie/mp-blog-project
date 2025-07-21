c05c9b64f4d7c14d3d52580fcf993195ffdaa2fc
manprajapatie <manprajapatie@gmail.com>
2025-07-21 23:28:44 +0530

    add protection for routes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,17 +1,43 @@
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from '../features/recipes/pages/Home'
 import RecipeDetails from '../features/recipes/pages/RecipeDetails'
 import Favorites from '../features/recipes/pages/Favorites'
+import Login from '../features/auth/pages/login'
+import ProtectedRoute from '../components/protectedRoute'
 
 
 const AppRoutes = () => {
   return (
     <>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/Favorites' element={<Favorites/>}/>
-        <Route path='/RecipeDetails/:id' element={<RecipeDetails/>}/>
+        <Route path='/'
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          } />
+
+        <Route path='/Login'
+          element={
+            <Login />
+          } />
+
+        <Route path='/Favorites'
+          element={
+            <ProtectedRoute>
+              <Favorites />
+            </ProtectedRoute>
+
+          } />
+
+        <Route path='/RecipeDetails/:id'
+          element={
+            <ProtectedRoute>
+              <RecipeDetails />
+            </ProtectedRoute>
+          } />
+
       </Routes>
     </>
   )
