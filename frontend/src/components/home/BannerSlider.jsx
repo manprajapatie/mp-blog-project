a6fc88b47ef3350c5fc30a933ca8c9f935628ea7
manprajapatie <manprajapatie@gmail.com>
2025-07-25 21:31:03 +0530

    Style BannerSlider Img

---
+++ frontend/src/components/home/BannerSlider.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import img1 from "../../assets/FoodImg/FoodImg1.png"
+import img2 from "../../assets/FoodImg/FoodImg2.png"
+import img3 from "../../assets/FoodImg/FoodImg3.png"
+import img4 from "../../assets/FoodImg/FoodImg4.png"
+
+const BannerSlider = () => {
+
+  const img = [
+    { src: img1, Title: "Chowmain", Detail: " lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd  " },
+    { src: img2, Title: "Man", Detail: " lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd  " },
+    { src: img3, Title: "Prajapati", Detail: " lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd  " },
+    { src: img4, Title: "Hai yaha per", Detail: " lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd lorem12asdf asd  " },
+  ]
+  const [index, setIndex] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(
+      () => {
+        setIndex((i) => (i + 1) % img.length)
+      }, 5000);
+    return () => clearInterval(interval)
+  }, [])
+
+
+  return (
+
+    <>
+      <section className='flex max-w-full w-full h-[43rem] border-solid border-2 border-Secondary-100 justify-end relative'>
+
+        {/* Container For Right Design */}
+        <div className='max-w-[60%] w-full h-full bg-primary-800 rounded-bl-[15rem] flex justify-center items-center'>
+        </div>
+
+        {/* Container For Text and Image */}
+
+        <div className='flex justify-between max-w-full w-full h-[43rem] border-solid border-2 border-Secondary-100 absolute items-center'>
+
+          {/* Text Area */}
+          <div className=' border-solid border-2 border-black w-[30%] h-1/2 m-5 p-2 '>
+            <h2 className='font-Rouge text-4xl text-primary-800 pb-3'>Namasty</h2>
+            <h1 className='font-Lora text-4xl bold pb-4'>Fresh Food <span className='text-primary-800'>Low Price</span></h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores, reiciendis? In, deleniti!
+          </p>
+          <button></button>
+          </div>
+
+          {/* Image Area */}
+
+
+
+          <div className='border-solid border-2 border-black h-3/4 flex justify-center items-center grow-10'>
+            <img src={img[index].src} alt="" className='h-[70%]  ZoomInOut' />
+          </div>
+
+        </div>
+      </section>
+
+    </>
+  )
+}
+
+export default BannerSlider
