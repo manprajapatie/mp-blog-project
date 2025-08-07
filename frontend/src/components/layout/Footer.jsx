cfc5dbfbb6560d1b2a93f6aaa6dec0999d4cb0f5
manprajapatie <manprajapatie@gmail.com>
2025-08-07 22:31:23 +0530

    complete footer section

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -3,9 +3,33 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-      <footer className="bg-gray-800 text-white text-center p-4">
-      <p>&copy; 2025 Recipe Blog. All rights reserved.</p>
-    </footer>
+      <footer className="bg-gray-800 text-white text-center p-7">
+        <div className='w-full h-35 bg-gray-900 grid grid-cols-3 p-5'>
+
+          {/* Call us*/}
+          
+          <div className='  flex justify-center items-center flex-col '>
+            <h1 className='text-3xl mb-4 '>Call Us On :</h1>
+            <p className='text-primary-800'>+91 1111111110515</p>
+          </div>
+
+          {/* Opening Hour */}
+
+          <div className='  flex justify-center items-center flex-col '>
+            <h1 className='text-3xl mb-4 '>Opening Hours :</h1>
+            <p className='text-primary-800'>10:00 AM - 10:00PM</p>
+          </div>
+
+          {/* Mail Us */}
+
+          <div className='  flex justify-center items-center flex-col '>
+            <h1 className='text-3xl mb-4 '> Email Us :</h1>
+            <p className='text-primary-800'>manprajapatie@gmail.com</p>
+          </div>
+
+        </div>
+        <p className='pt-4'>&copy; 2025 Recipe Blog. All rights reserved.</p>
+      </footer>
     </>
   )
 }
