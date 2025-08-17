8b96a120781f241df55a5fd7d815bc1f3c66432b
manprajapatie <manprajapatie@gmail.com>
2025-08-17 21:51:53 +0530

    make footer responsive

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -4,26 +4,26 @@ const Footer = () => {
   return (
     <>
       <footer className="bg-gray-800 text-white text-center p-7">
-        <div className='w-full h-35 bg-gray-900 grid grid-cols-3 p-5'>
+        <div className='w-full bg-gray-900 grid grid-cols-1 sm:grid-cols-3 p-5'>
 
           {/* Call us*/}
           
-          <div className='  flex justify-center items-center flex-col '>
-            <h1 className='text-3xl mb-4 '>Call Us On :</h1>
+          <div className='  flex justify-center items-center flex-col m-3'>
+            <h1 className='text-xl md:text-3xl mb-2 sm:mb-4  '>Call Us On :</h1>
             <p className='text-primary-800'>+91 1111111110515</p>
           </div>
 
           {/* Opening Hour */}
 
-          <div className='  flex justify-center items-center flex-col '>
-            <h1 className='text-3xl mb-4 '>Opening Hours :</h1>
+          <div className='  flex justify-center items-center flex-col m-3'>
+            <h1 className='text-xl md:text-3xl mb-2 sm:mb-4 '>Opening Hours :</h1>
             <p className='text-primary-800'>10:00 AM - 10:00PM</p>
           </div>
 
           {/* Mail Us */}
 
-          <div className='  flex justify-center items-center flex-col '>
-            <h1 className='text-3xl mb-4 '> Email Us :</h1>
+          <div className='  flex justify-center items-center flex-col m-3'>
+            <h1 className='text-xl md:text-3xl mb-2 sm:mb-4 '> Email Us :</h1>
             <p className='text-primary-800'>manprajapatie@gmail.com</p>
           </div>
 
