8d0e8e6c059050a874f81f434817d5de881da677
manprajapatie <manprajapatie@gmail.com>
2025-09-22 07:18:24 +0530

    add useEffect for api call of product in Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,9 +6,15 @@ import { useDebounce } from '../utils/useDebounce'
 import BannerSlider from '../components/home/BannerSlider'
 import AboutUs from '../components/home/AboutUs'
 import RandomRecipes from '../components/home/RandomRecipes'
+import { fetchApi } from '../features/recipes/recipeSlice'
 
 
 const Home = () => {
+
+  const dispatch = useDispatch()
+   useEffect(() => {
+    dispatch(fetchApi());
+  }, [dispatch]);
 
   return (
     <>
