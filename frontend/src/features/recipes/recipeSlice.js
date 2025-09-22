c7c477f8eb11368bf003f1814ef6c38dcfd04daf
manprajapatie <manprajapatie@gmail.com>
2025-09-22 07:17:35 +0530

    fetch api throgh React Tool Kit

--- frontend/src/features/recipes/recipeSlice.js
+++ frontend/src/features/recipes/recipeSlice.js
@@ -1,18 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { recipes as staticData } from "../../constants/recipes";
+
+//api call throgh reduxToolkit
+export const fetchApi = createAsyncThunk("fetchApi", async () => {
+    const response = await fetch('https://dummyjson.com/recipes')
+    const data = await response.json();
+    return data.recipes;
+})
+
 
 const initialState = {
     all: staticData,
+    isLoading: false,
+    isError: false,
     favorites: [],
 };
+console.log(`initialState >>>>>>>>>>>>> ${initialState}`);
 
 const recipeSlice = createSlice({
     name: 'recipes',
     initialState,
     reducers: {
-        setRecipes: (state, action) => {
-            state.all = action.payload;
-        },
+
         addToFavorites: (state, action) => {
             if (!state.favorites.includes(action.payload)) {
                 state.favorites.push(action.payload)
@@ -21,7 +30,22 @@ const recipeSlice = createSlice({
         removeFromFavorites: (state, action) => {
             state.favorites = state.favorites.filter((id) => id !== action.payload);
         },
+
     },
+
+    extraReducers: (builder) => {
+        builder.addCase(fetchApi.fulfilled, (state, action) => {
+            state.all = action.payload
+        });
+        builder.addCase(fetchApi.pending, (state, action) => {
+            state.isLoading = true
+        });
+        builder.addCase(fetchApi.rejected, (state, action) => {
+            console.log('Error in Api call (in redux)', action.payload);
+
+        });
+    },
+
 });
 
 export const { setRecipes, addToFavorites, removeFromFavorites } = recipeSlice.actions;
