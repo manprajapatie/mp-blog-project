0cd0707ec8974d237e8ae492849ddd0f3e0cf67e
manprajapatie <manprajapatie@gmail.com>
2025-09-22 07:54:27 +0530

    remove dispatch api from home and put it into app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
-import { useState } from 'react'
-import Header from '../src/components/Header'
-import Footer from '../src/components/Footer'
+import { useEffect, useState } from 'react'
+import Header from './components/layout/Header'
+import Footer from '../src/components/layout/Footer'
 import AppRoutes from '../src/routes/AppRoutes'
 import './App.css'
+import { fetchApi } from './features/recipes/recipeSlice'
+import { useDispatch } from 'react-redux'
 
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+    const dispatch = useDispatch()
+   useEffect(() => {
+    dispatch(fetchApi());
+  }, [dispatch]);
+
 
   return (
     <>
