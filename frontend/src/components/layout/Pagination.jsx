50bd8e2947f8b3f28ee635a2dfd0884f5746ce34
manprajapatie <manprajapatie@gmail.com>
2025-12-17 21:48:55 +0530

    fix some pagination logic

--- frontend/src/components/layout/Pagination.jsx
+++ frontend/src/components/layout/Pagination.jsx
@@ -26,6 +26,7 @@ const Pagination = ({ currentPage, totalPages, setCurrentPage }) => {
                     page === "..." ? (<span key={index} className="px-4 py-2 text-gray-500">...</span>)
                         : (<button
                             key={page}
+                            onClick={() => setCurrentPage(page)}
                             className={`px-4 py-2 rounded ${page === currentPage ? "bg-blue-500 text-white" : "bg-white text-black"
                                 }`}
                         >
@@ -35,9 +36,9 @@ const Pagination = ({ currentPage, totalPages, setCurrentPage }) => {
 
                 {/* Previes Button */}
                 <button
-                className='px-4 py-2 rounded bg-gray-300 disabled:opacity-50'
-                onClick={()=> setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
-                disabled={currentPage === totalPages}
+                    className='px-4 py-2 rounded bg-gray-300 disabled:opacity-50'
+                    onClick={() => setCurrentPage((prev) => Math.min(prev + 1, totalPages))}
+                    disabled={currentPage === totalPages}
                 >
                     next
                 </button>
